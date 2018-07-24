448641d734b2d683ad6888341d879d6ab3261bef
Chase Whitney <ChaseEWhitney@gmail.com>
2018-07-24 14:28:04 -0500

    concept of target

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-
-class MyMessage extends React.Component {
+//import './index.css';
+//import {Child} from './anotherFile.js';
+console.log('index.js loaded');
+class MyStuff extends React.Component{
+  colorClick(e){
+    console.log('you clicked:', e.target.value);
+  }
   render(){
-    return <h1>Hello {this.props.name}</h1>;
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <h2>or Hello {this.props.name}</h2>
+        <select onChange={this.colorClick}>
+          <option value="blue" onClick={this.colorClick}>blue</option>
+          <option value="red" onClick={this.colorClick}>red</option>
+          <option value="green" onClick={this.colorClick}>green</option>
+        </select>
+      </div>
+    );
   }
 }
 
-ReactDOM.render(<MyMessage name="World"/>, document.getElementById('root'));
+ReactDOM.render(<MyStuff name="AnotherWorld"/>, document.getElementById('root'));
