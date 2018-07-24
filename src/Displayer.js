de1107f5b5cced36b6852daf4ecd40d19181db27
Chase Whitney <ChaseEWhitney@gmail.com>
2018-07-24 17:16:29 -0500

    learning pattern: child update pparent state, parent passes state to sibling component

--- src/Displayer.js
+++ src/Displayer.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-export class Child extends React.Component {
+export class Displayer extends React.Component {
+
   render(){
+    const name = this.props.worldName;
     return (
-    <h1></h1>
+    <h1>Hello {name}</h1>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,31 +2,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 //import './index.css';
-//import {Child} from './anotherFile.js';
-console.log('index.js loaded');
+import {Changer} from './Changer.js';
+import {Displayer} from './Displayer.js';
+
 class MyStuff extends React.Component{
   constructor(props){
     super(props);
-    this.state = {worldName : 'World'}
-    this.colorClick = this.colorClick.bind(this);
+    this.state = {worldName : 'Blue World'};
+    this.changeName = this.changeName.bind(this);
   }
 
-
-  colorClick(e){
-    console.log('you clicked:', e.target.value);
-    const newName = e.target.value;
-    this.setState({worldName: newName});
-
+  changeName(newName){
+    console.log('in changeName with:', newName);
+    this.setState({worldName : newName});
   }
+
   render(){
     return (
       <div>
-        <h1>Hello {this.state.worldName}</h1>
-        <select onChange={this.colorClick}>
-          <option value="Blue World" onClick={this.colorClick}>blue</option>
-          <option value="Red World" onClick={this.colorClick}>red</option>
-          <option value="Green World" onClick={this.colorClick}>green</option>
-        </select>
+        <Displayer worldName={this.state.worldName}/>
+        <Changer changerMethod={this.changeName}/>
       </div>
     );
   }
